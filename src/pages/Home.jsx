bd78e05a9dd9238cb9c2c37657052ea06ad8ef67
soniagkalliath <soniageorge111@gmail.com>
2026-04-27 12:59:46 +0530

    ADD: path, design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div style={{height:'400px',background:'url("https://img.freepik.com/premium-photo/banner-talking-business-people-meeting-work-discussion-workshop-teamwork-morning-training-black-man-speaking-employees-about-agenda-schedule-planning-together_590464-228765.jpg")',backgroundSize:'cover'}}></div>
+    </div>
   )
 }
 
