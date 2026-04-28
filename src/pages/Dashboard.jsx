491cd9acb52af5bafbce32cc289f179459557971
soniagkalliath <soniageorge111@gmail.com>
2026-04-28 13:00:18 +0530

    ADD: userdata

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      {/* title */}
+      <div className="d-flex justify-content-between m-5 align-items-center">
+        {/* welcome */}
+        <h1>Welcome Admin,</h1>
+        {/* add user link */}
+        <Link to={'/add'} className='btn btn-primary'>+ ADD USER</Link>
+      </div>
+      <div className="m-5">
+        <h3 className='text-center text-warning'>All Users List</h3>
+        <table className="table table-hover my-5">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Salary</th>
+              <th>...</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>username</td>
+              <td>email</td>
+              <td>salary</td>
+              <td>
+                <div className="d-flex">
+                  <Link to={'/id/edit'} className='btn text-warning'> <i className='fa-solid fa-edit'></i> </Link>
+                  <button className="btn text-danger"> <i className='fa-solid fa-trash'></i> </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
 //rfce
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
       <div style={{height:'400px',background:'url("https://img.freepik.com/premium-photo/banner-talking-business-people-meeting-work-discussion-workshop-teamwork-morning-training-black-man-speaking-employees-about-agenda-schedule-planning-together_590464-228765.jpg")',backgroundSize:'cover'}}></div>
+      {/* section admin intro */}
+      <div className="container my-5">
+        <div className="row align-items-center">
+          <div className="col-lg-6">
+            {/* text content */}
+            <p style={{textAlign:'justify'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus sunt nemo numquam? Velit porro perferendis dolor id debitis cum quia eligendi ullam voluptas perspiciatis dolores eaque, iusto magnam animi aperiam!
+            Delectus quod necessitatibus id quibusdam cumque cum praesentium qui atque aspernatur, accusamus, consequuntur aliquid aperiam neque molestiae. Maiores, provident magni explicabo aperiam ullam labore veritatis consectetur temporibus, cum cupiditate natus.
+            Exercitationem ut perspiciatis, quasi dolorum maiores at aut nesciunt corrupti atque odio distinctio voluptates deleniti placeat cupiditate vitae eveniet nisi recusandae repellat sequi. Facilis reprehenderit quasi voluptatibus perferendis, ratione sequi.
+            Mollitia deleniti ab dolor similique expedita perspiciatis quas in quia. Numquam accusantium quis quas provident perspiciatis impedit libero molestias, ipsam dicta tempore? Sint praesentium quaerat consequuntur temporibus modi non placeat!</p>
+          </div>
+          <div className="col-lg-6">
+            {/* image */}
+            <img className='img-fluid' src="https://static.vecteezy.com/system/resources/thumbnails/026/497/734/small_2x/businessman-on-isolated-png.png" alt="profile" />
+          </div>
+        </div>
+        <div className=" text-center my-5">
+          <Link to={'/admin'} className='btn btn-info'>GO TO DASHBOARD</Link>
+        </div>
+      </div>
     </div>
   )
 }
